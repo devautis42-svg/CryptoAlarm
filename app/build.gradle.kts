d10529e9aee7b8a2c16735c9b7ae114b74d5cf87
devautis42-svg <devautis42@gmail.com>
2026-09-15 01:26:08 +0300

    Bump version to 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.cryptoalarm.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.0.0"
+        versionCode = 11
+        versionName = "1.1.0"
     }
 
     buildFeatures { compose = true }
